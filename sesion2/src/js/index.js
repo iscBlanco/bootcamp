2dba558109f975f62a3fc1961ff7a3f2c4863c2d
Blanco <Blanco@vets-MacBook-Pro.local>
2019-12-02 19:05:21 -0600

    [add] la clase 4 sesion 2

--- sesion2/src/js/index.js
+++ sesion2/src/js/index.js
@@ -4,8 +4,21 @@
     let ej01Button = document.querySelector('#Sesion2 > button')
     let resultadoNodo = document.querySelector('#resultado')
     //console.log("dentro de division formateada")
+    let ej02section = document.querySelector('#parrafos')
+    let ej03section = document.querySelector('#ejercicio03')
 
-
+    //ej03section.addEventListener('mouseover',function(){
+      //  ej03section.className = ('sectionAnimated')
+        //console.log('Estamos en linea')
+    //})
+    ej03section.addEventListener('mouseover',function(){
+        ej03section.classList.add ('sectionAnimated')
+        //console.log('Estamos en linea')
+    })
+    ej03section.addEventListener('click',function(){
+        ej03section.classList.remove ('sectionAnimated')
+        //console.log('Estamos en linea')
+    })
 
     ej01Button.addEventListener('click',function(){
         let res = divisionFormatted(parseInt(n1Val.value),parseInt(n2Val.value))
@@ -22,11 +35,21 @@
 
     })
 
+    ej02section.addEventListener('animationstart',function(){
+        console.log('animation has started...')
+    })
 
+    ej02section.addEventListener('animationend',function(){
+        console.log('animation has ended...')
+    })
 
+    ej02section.addEventListener('animationiteration',function(){
+        console.log('animation loop...')
+    })
 
-
-
+    ej02section.addEventListener('animationend',function(){
+        ej02section.classList.add('ocultarElemento')
+    })
 
 
     function divisionFormatted(n1,n2){
