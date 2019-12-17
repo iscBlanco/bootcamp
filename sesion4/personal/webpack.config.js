963a27328594d7e27ef930b484a31ced5bfde33b
Blanco <Blanco@vets-MacBook-Pro.local>
2019-12-17 00:37:42 -0600

    [add] sesion 4

---
+++ sesion4/personal/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+let extractTextPlugin = new ExtractTextPlugin({filename: 'css/style.css'})
+module.exports = {
+  entry: {
+    app: './src/js/index.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    extractTextPlugin,
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      //{
+        //test: /\.css$/,
+        //use: ['style-loader', 'css-loader']
+      //}
+      {
+        test:/\.scss$/,
+        use: extractTextPlugin.extract({
+          use:['css-loader','sass-loader']
+        })
+        
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  }
+}
